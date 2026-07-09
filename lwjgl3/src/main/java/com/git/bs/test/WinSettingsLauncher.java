374f34937a31f36055d9e599748aba1878271438
authorZhao <44185685+authorZhao@users.noreply.github.com>
2026-07-09 18:41:59 +0800

    Create WinSettingsLauncher.java

---
+++ lwjgl3/src/main/java/com/git/bs/test/WinSettingsLauncher.java
@@ -0,0 +1,26 @@
+package com.git.bs.test;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.git.bs.common.PlatformStatic;
+import com.git.bs.common.impl.DeskPlatform;
+import com.git.bs.winsettings.WinSettingsApp;
+
+/**
+ * Windows 设置启动器。
+ * <p>IDEA 右键 Run；或 {@code ./gradlew :lwjgl3:run -PmainClass=com.git.bs.test.WinSettingsLauncher}。</p>
+ */
+public class WinSettingsLauncher {
+
+    public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Windows 设置 (bs-ui)");
+        config.setWindowedMode(1100, 720);
+        config.useVsync(true);
+        config.setSamples(4);
+        config.setIdleFPS(60);
+        config.setForegroundFPS(60);
+        PlatformStatic.registerImpl(DeskPlatform.class);
+        new Lwjgl3Application(new WinSettingsApp(), config);
+    }
+}
