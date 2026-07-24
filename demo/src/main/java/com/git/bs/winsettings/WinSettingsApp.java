160fc3315d93eb64e3be26e1f10ef13cd95bc5bb
authorZhao <44185685+authorZhao@users.noreply.github.com>
2026-07-24 18:32:48 +0800

    Update WinSettingsApp.java

--- demo/src/main/java/com/git/bs/winsettings/WinSettingsApp.java
+++ demo/src/main/java/com/git/bs/winsettings/WinSettingsApp.java
@@ -27,21 +27,9 @@ public class WinSettingsApp extends Game {
 
     @Override
     public void create() {
-        // P1 spike：资源 pak 包装（-Dbs.pak.spike=true 启用）。必须在 BsUI.init() 之前。
+        // 资源 pak 加解密（必须在 BsUI.init 之前；无 pak 时自动跳过，走明文 classpath）
         com.git.bs.res.PakBootstrap.init();
         BsUI.init();
-        // P1 spike 观测：皮肤加载后，打印从内存包服务过的路径（空 = 未启用 spike）
-        java.util.Set<String> served = com.git.bs.res.PakFileHandle.servedPaths();
-        log.info("[pak-spike] 已从内存包服务 {} 个资源：", served.size());
-        served.forEach(p -> log.info("[pak-spike]   pak> {}", p));
-        // spike 验证：bs.pak.spike=exit 或 bs.exit=true 时，打印后 1s 自动退出（无界面拿干净日志）
-        boolean doExit = "exit".equalsIgnoreCase(System.getProperty("bs.pak.spike"))
-                || "true".equalsIgnoreCase(System.getProperty("bs.exit"));
-        if (doExit) {
-            com.badlogic.gdx.utils.Timer.schedule(new com.badlogic.gdx.utils.Timer.Task() {
-                @Override public void run() { Gdx.app.exit(); }
-            }, 1f);
-        }
         // 注册 winsettings 业务翻译包（core 自带的通用 key 之外，业务专属 key 在这里）
         BsI18n.addBundle("com/git/bs/demo/i18n/");
         BsI18n.init();                          // i18n：加载 core + demo 业务包，默认 zh_cn
